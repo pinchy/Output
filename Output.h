c0c08a000c870fa4cd8f877fe2ff7dc7845881d4
pinchy <sebeckmas@gmail.com>
2022-10-18 22:34:00 +1100

    doxygen

--- Output.h
+++ Output.h
@@ -18,52 +18,82 @@ class Output
         int _invert;
 
     public:
-        /*
-         * Constructor, initaite the Output object on Pin p and set the state to
+        /**
+         * @brief Initaite the Output object on Pin p and set the state to
          * s and invert the output
+         *
+         * @param p the target pin
+         * @param s the initial state (default LOW)
+         * @param i invert the output (default false)
          */
         Output(int p, int s = false, bool i = false);
+
+        /**
+         * @brief Initialise the output object. Need to call setPin() prior to use
+         *
+         */
         Output();
 
-        /*
-         * Set the pin, initaite the Output object on Pin p and set the state to
-         * s and invert the output
+        /**
+         * @brief Set the pin, initial state and inverse state
+         * @param p the target pin
+         * @param s the initial state (default LOW)
+         * @param i invert the output (default false)
          */
         void setPin(int p, int s = false, bool i = false);
 
+        /**
+         * @brief get the pin number
+         * @return (int) the pin number
+         */
         int getPin(void) { return this->_pin; }
+
+
         /*
          * Set the output to the passed variable (ON or OFF) and update
          * the internal value
          */
+
+        /**
+         * @brief Set the output to the passed state
+         * @param s the state to set the output to
+         */
         void set(int s);
 
-        /*
-         * Toggle (flip) the state of the pin and update the intenral variable
+        /**
+         * @brief Toggle (flip) the state of the pin
          */
         void toggle(void);
 
-        /*
-         * get the state (as represented by the internal variable)
+        /**
+         * @brief get the state (as represented by the internal variable)
          * Note: This could be different to the pin if another service has
          * affected the pin directly.
+         * @return (int) the internal state
          */
         int getState(void);
 
-        /*
-         * check the state against the actual pin, update the internal
+        /**
+         * @brief check the state against the actual pin, update the internal
          * variable and return this state
+         * @return (int) the pin state
          */
         int checkState(void);
 
         /*
          * Set the state on
          */
+
+        /**
+         * @brief Set the pin to ON/HIGH
+         *
+         */
         void on(void);
         void open(void);
 
-        /*
-         * Set the state off
+        /**
+         * @brief Set the pin to OFF/LOW
+         *
          */
         void off(void);
         void close(void);
